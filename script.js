8b4e445b5001f1a3e5c8a16f46f9fcba7d40de05
ahsatal-pierre <97099978+ahsatal-pierre@users.noreply.github.com>
2023-05-14 14:56:16 +0200

    removed comment script.js

--- script.js
+++ script.js
@@ -78,20 +78,3 @@ canvas.height = window.innerHeight;
 
 
 
-/* 
-
-
-
-const katakana =
-  "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン";
-const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const nums = "0123456789";
-const alphabet = katakana + latin + nums;
-const fontSize = 16;
-const columns = canvas.width/fontSize;
-
-
-
-setInterval(draw, 30);
-
- */
